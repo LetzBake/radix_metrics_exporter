b74c2bbaf08c1114060663358a55ae2c6fbe8349
Stuart Bain <stuart.bain@gmail.com>
2021-05-05 12:45:36 +0100

    Remove duplicated code Refactor Prometheus gauge initialisation

--- radix_info.go
+++ radix_info.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/jeremywohl/flatten"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/tidwall/gjson"
@@ -10,12 +11,45 @@ import (
 	"log"
 	"net/http"
 	"time"
-	"fmt"
 )
 
 var (
 	registry = prometheus.NewRegistry()
+
+	radix_validator_peers_count = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_peers_count",
+		Help: "Count of Validator Peers",
+	})
+
+	radix_validator_next_validators_count = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_next_validators_count",
+	})
+
+	radix_validator_next_validators_stake_min = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_next_validators_stake_min",
+	})
+
+	radix_validator_next_validators_stake_max = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_next_validators_stake_max",
+	})
+
+	radix_validator_stake_total = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_stake_total",
+	})
+
+	radix_validator_delegators_count = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "radix_validator_delegators_count",
+	})
 )
+
+func init() {
+	registry.MustRegister(radix_validator_peers_count)
+	registry.MustRegister(radix_validator_next_validators_count)
+	registry.MustRegister(radix_validator_next_validators_stake_min)
+	registry.MustRegister(radix_validator_next_validators_stake_max)
+	registry.MustRegister(radix_validator_stake_total)
+	registry.MustRegister(radix_validator_delegators_count)
+}
 
 func main() {
 	var baseUrl string
@@ -51,20 +85,7 @@ func newClient() *http.Client {
 
 func systemInfo(baseUrl string) {
 	url := baseUrl + "/system/info"
-
-	r, getErr := newClient().Get(url)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	body, readErr := ioutil.ReadAll(r.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
+	body := getData(url)
 
 	var info map[string]interface{}
 	jsonErr := json.Unmarshal(body, &info)
@@ -86,6 +107,7 @@ func systemInfo(baseUrl string) {
 	delete(flat, "radix_info_configuration_pacemakerTimeout")
 	delete(flat, "radix_info_configuration_pacemakerMaxExponent")
 
+	// Dynamically create Gauges
 	for key, val := range flat {
 		v, ok := val.(float64)
 		if ok {
@@ -98,7 +120,43 @@ func systemInfo(baseUrl string) {
 
 func systemPeers(baseUrl string) {
 	url := baseUrl + "/system/peers"
+	body := getData(url)
 
+	var peers []interface{}
+	jsonErr := json.Unmarshal(body, &peers)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	radix_validator_peers_count.Set(float64(len(peers)))
+}
+
+func systemEpochproof(baseUrl string) {
+	url := baseUrl + "/system/epochproof"
+	body := getData(url)
+
+	result := gjson.GetBytes(body, "header.nextValidators.#.stake")
+
+	nextValidators := result.Array()
+	minStake, maxStake := minMax(nextValidators)
+
+	radix_validator_next_validators_count.Set(float64(len(nextValidators)))
+	radix_validator_next_validators_stake_min.Set((minStake / 1e18))
+	radix_validator_next_validators_stake_max.Set((maxStake / 1e18))
+}
+
+func nodeValidator(baseUrl string) {
+	url := baseUrl + "/node/validator"
+	body := postData(url)
+
+	totalStakes := gjson.GetBytes(body, "validator.totalStake").Float()
+	stakes := gjson.GetBytes(body, "validator.stakes").Array()
+
+	radix_validator_stake_total.Set(totalStakes)
+	radix_validator_delegators_count.Set(float64(len(stakes)))
+}
+
+func getData(url string) []byte {
 	r, getErr := newClient().Get(url)
 	if getErr != nil {
 		log.Fatal(getErr)
@@ -113,75 +171,10 @@ func systemPeers(baseUrl string) {
 		log.Fatal(readErr)
 	}
 
-	var peers []interface{}
-	jsonErr := json.Unmarshal(body, &peers)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	radix_validator_peers_count := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_peers_count",
-		Help: "Count of Validator Peers",
-	})
-	registry.MustRegister(radix_validator_peers_count)
-	radix_validator_peers_count.Set(float64(len(peers)))
+	return body
 }
 
-func systemEpochproof(baseUrl string) {
-	url := baseUrl + "/system/epochproof"
-
-	radix_validator_next_validators_count := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_next_validators_count",
-	})
-	registry.MustRegister(radix_validator_next_validators_count)
-
-	radix_validator_next_validators_stake_min := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_next_validators_stake_min",
-	})
-	registry.MustRegister(radix_validator_next_validators_stake_min)
-
-	radix_validator_next_validators_stake_max := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_next_validators_stake_max",
-	})
-	registry.MustRegister(radix_validator_next_validators_stake_max)
-
-	r, getErr := newClient().Get(url)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	json, readErr := ioutil.ReadAll(r.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	result := gjson.GetBytes(json, "header.nextValidators.#.stake")
-
-	nextValidators := result.Array()
-	minStake, maxStake := minMax(nextValidators)
-
-	radix_validator_next_validators_count.Set(float64(len(nextValidators)))
-	radix_validator_next_validators_stake_min.Set((minStake / 1e18))
-	radix_validator_next_validators_stake_max.Set((maxStake / 1e18))
-}
-
-func nodeValidator(baseUrl string) {
-	url := baseUrl + "/node/validator"
-
-	radix_validator_stake_total := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_stake_total",
-	})
-	registry.MustRegister(radix_validator_stake_total)
-
-	radix_validator_delegators_count := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "radix_validator_delegators_count",
-	})
-	registry.MustRegister(radix_validator_delegators_count)
-
+func postData(url string) []byte {
 	r, postErr := newClient().Post(url, "application/json", nil)
 	if postErr != nil {
 		log.Fatal(postErr)
@@ -191,16 +184,12 @@ func nodeValidator(baseUrl string) {
 		defer r.Body.Close()
 	}
 
-	json, readErr := ioutil.ReadAll(r.Body)
+	body, readErr := ioutil.ReadAll(r.Body)
 	if readErr != nil {
 		log.Fatal(readErr)
 	}
 
-	totalStakes := gjson.GetBytes(json, "validator.totalStake").Float()
-	stakes := gjson.GetBytes(json, "validator.stakes").Array()
-
-	radix_validator_stake_total.Set(totalStakes)
-	radix_validator_delegators_count.Set(float64(len(stakes)))
+	return body
 }
 
 func minMax(array []gjson.Result) (float64, float64) {
